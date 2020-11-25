bfd63a31c89b075e25e745a68190a9c5d11bb250
Elliott Regan <elliott@elliottregan.com>
2020-11-24 22:35:33 -0700

    tuning the animation

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -3,40 +3,45 @@ import anime from 'animejs';
 
 import './Background.scss';
 
-const defaults = {
-  duration: 4000
+const SWAY_DEFAULTS = {
+  duration: 8000
 }
 
-const useHorizontalAnimation = (target: React.RefObject<SVGSVGElement> | React.RefObject<SVGPathElement>, options: { duration: any; } = defaults) => {
+const PATH_DEFAULTS = {
+  duration: 16000
+}
+
+const useHorizontalAnimation = (
+  target: React.RefObject<SVGSVGElement> | React.RefObject<SVGPathElement>, options: { duration: number; } = SWAY_DEFAULTS) => {
   const animationRef = useRef(anime({}));
   useEffect(() => {
     animationRef.current = anime({
       targets: target.current,
-      translateX: '25%',
-      duration: 8000,
+      translateX: '15%',
+      translateY: '5%',
+      duration: options.duration,
       loop: true,
       direction: "alternate",
-      easing: "easeInOutSine"
+      easing: "easeInOutBack",
+      delay: 0,
     });
-    animationRef.current.play()
 
   }, [target, options]);
 
   return animationRef;
 }
 
-const useBackgroundAnimation = (target: React.RefObject<SVGSVGElement> | React.RefObject<SVGPathElement>, options: { duration: any; } = defaults) => {
+const useBackgroundAnimation = (target: React.RefObject<SVGSVGElement> | React.RefObject<SVGPathElement>, options: { duration: number; } = PATH_DEFAULTS) => {
   const animationRef = useRef(anime({}));
   useEffect(() => {
     animationRef.current = anime({
       targets: target.current,
       duration: options.duration,
-      easing: 'easeInOutSine',
+      easing: 'easeInOutBack',
       d: target.current?.dataset.altD,
       loop: true,
       direction: "alternate",
     });
-    animationRef.current.play()
 
   }, [target, options]);
 
@@ -47,12 +52,17 @@ function Background() {
 
   const shape1 = useRef<SVGSVGElement>(null);
   const shape2 = useRef<SVGSVGElement>(null);
+  const shape3 = useRef<SVGSVGElement>(null);
   const path1 = useRef<SVGPathElement>(null);
   const path2 = useRef<SVGPathElement>(null);
+  const path3 = useRef<SVGPathElement>(null);
 
   useHorizontalAnimation(shape1);
+  useHorizontalAnimation(shape2, { duration: 10000 });
+  useHorizontalAnimation(shape3, { duration: 12000 });
   useBackgroundAnimation(path1);
-  useBackgroundAnimation(path2, { duration: 5000 });
+  useBackgroundAnimation(path2);
+  useBackgroundAnimation(path3);
 
   return (
     <div>
@@ -62,17 +72,26 @@ function Background() {
             ref={path1}
             fill="#0099ff"
             fillOpacity="0.65"
-            d="M0,192L48,176C96,160,192,128,288,128C384,128,480,160,576,149.3C672,139,768,85,864,101.3C960,117,1056,203,1152,213.3C1248,224,1344,160,1392,128L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-            data-alt-d="M0,128L48,122.7C96,117,192,107,288,128C384,149,480,203,576,224C672,245,768,235,864,208C960,181,1056,139,1152,133.3C1248,128,1344,160,1392,176L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            d="M0,256L60,256C120,256,240,256,360,229.3C480,203,600,149,720,138.7C840,128,960,160,1080,165.3C1200,171,1320,149,1380,138.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+            data-alt-d="M0,128L60,133.3C120,139,240,149,360,133.3C480,117,600,75,720,80C840,85,960,139,1080,144C1200,149,1320,107,1380,85.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
           ></path>
         </svg>
         <svg ref={shape2} className="shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             ref={path2}
             fill="#0099ff"
-            fillOpacity="0.65"
+            fillOpacity="0.25"
             d="M0,192L48,197.3C96,203,192,213,288,202.7C384,192,480,160,576,165.3C672,171,768,213,864,213.3C960,213,1056,171,1152,170.7C1248,171,1344,213,1392,234.7L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
             data-alt-d="M0,224L48,218.7C96,213,192,203,288,213.3C384,224,480,256,576,261.3C672,267,768,245,864,245.3C960,245,1056,267,1152,272C1248,277,1344,267,1392,261.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          ></path>
+        </svg>
+        <svg ref={shape3} className="shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            ref={path3}
+            fill="#0099ff"
+            fillOpacity="0.15"
+            d="M0,256L60,240C120,224,240,192,360,192C480,192,600,224,720,224C840,224,960,192,1080,181.3C1200,171,1320,181,1380,186.7L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+            data-alt-d="M0,160L60,176C120,192,240,224,360,240C480,256,600,256,720,256C840,256,960,256,1080,240C1200,224,1320,192,1380,176L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
           ></path>
         </svg>
       </div>
